934f30c7e1383be48a0ed915e26eb41dc2c85d75
Bar <barzada1@gmail.com>
2023-05-13 21:30:36 +0300

    calender pixer

--- app/src/main/java/com/example/invitations/MainActivity.java
+++ app/src/main/java/com/example/invitations/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.invitations;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.Manifest;
+import android.app.TimePickerDialog;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
@@ -17,7 +18,25 @@ import android.provider.CalendarContract;
 import android.net.Uri;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.Calendar;
+import java.util.Locale;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -33,7 +52,11 @@ import java.util.Locale;
 import java.util.TimeZone;
 
 public class MainActivity extends AppCompatActivity {
-
+private EditText titleEditText;
+    private EditText locationEditText;
+    private EditText  startDatetimeEditText;
+    private EditText  endDatetimeEditText;
+    private EditText descriptionEditText;
     private static final int PERMISSION_REQUEST_WRITE_CALENDAR = 1;
 
     @Override
@@ -41,7 +64,109 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button addEventButton = findViewById(R.id.addEventButton);
+        TextInputLayout titleInputLayout = findViewById(R.id.titleTextView);
+         titleEditText=titleInputLayout.getEditText();
+        TextInputLayout locationInputLayout = findViewById(R.id.locationTextView);
+         locationEditText=locationInputLayout.getEditText();
+        TextInputLayout  startDatetimeInputLayout = findViewById(R.id.startdatetimeTextView);
+          startDatetimeEditText=startDatetimeInputLayout.getEditText();
+        TextInputLayout  endDatetimeInputLayout = findViewById(R.id.enddatetimeTextView);
+       endDatetimeEditText=endDatetimeInputLayout.getEditText();
+        TextInputLayout  descriptionInputLayout = findViewById(R.id.descriptionTextView);
+         descriptionEditText=descriptionInputLayout.getEditText();
+
+
+        // Load and parse the JSON file
+        try {
+            String jsonString = loadJSONFromAsset("event.json");
+            JSONObject eventJson = new JSONObject(jsonString);
+
+            // Retrieve event details from JSON object
+            String title = eventJson.getString("title");
+            String location = eventJson.getString("location");
+            String startDateTime = eventJson.getString("startDateTime");
+            String endDateTime = eventJson.getString("endDateTime");
+            String description = eventJson.getString("description");
+            // Set the event details to the TextViews
+            titleEditText.setText(title);
+            locationEditText.setText(location);
+            startDatetimeEditText.setText(startDateTime);
+            startDatetimeEditText.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Calendar calendar = Calendar.getInstance();
+                    int year = calendar.get(Calendar.YEAR);
+                    int month = calendar.get(Calendar.MONTH);
+                    int dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
+                    int hour = calendar.get(Calendar.HOUR_OF_DAY);
+                    int minute = calendar.get(Calendar.MINUTE);
+
+                    DatePickerDialog datePickerDialog = new DatePickerDialog(MainActivity.this,
+                            new DatePickerDialog.OnDateSetListener() {
+                                @Override
+                                public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                                    TimePickerDialog timePickerDialog = new TimePickerDialog(MainActivity.this,
+                                            new TimePickerDialog.OnTimeSetListener() {
+                                                @Override
+                                                public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                                                    Calendar selectedCalendar = Calendar.getInstance();
+                                                    selectedCalendar.set(year, month, dayOfMonth, hourOfDay, minute);
+
+                                                    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss", Locale.getDefault());
+                                                    String selectedDateTime = dateFormat.format(selectedCalendar.getTime());
+                                                    startDatetimeEditText.setText(selectedDateTime);
+                                                }
+                                            }, hour, minute, false);
+
+                                    timePickerDialog.show();
+                                }
+                            }, year, month, dayOfMonth);
+
+                    datePickerDialog.show();
+                }
+            });
+            endDatetimeEditText.setText(endDateTime);
+            endDatetimeEditText.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Calendar calendar = Calendar.getInstance();
+                    int year = calendar.get(Calendar.YEAR);
+                    int month = calendar.get(Calendar.MONTH);
+                    int dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
+                    int hour = calendar.get(Calendar.HOUR_OF_DAY);
+                    int minute = calendar.get(Calendar.MINUTE);
+
+                    DatePickerDialog datePickerDialog = new DatePickerDialog(MainActivity.this,
+                            new DatePickerDialog.OnDateSetListener() {
+                                @Override
+                                public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                                    TimePickerDialog timePickerDialog = new TimePickerDialog(MainActivity.this,
+                                            new TimePickerDialog.OnTimeSetListener() {
+                                                @Override
+                                                public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                                                    Calendar selectedCalendar = Calendar.getInstance();
+                                                    selectedCalendar.set(year, month, dayOfMonth, hourOfDay, minute);
+
+                                                    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss", Locale.getDefault());
+                                                    String selectedDateTime = dateFormat.format(selectedCalendar.getTime());
+                                                    endDatetimeEditText.setText(selectedDateTime);
+                                                }
+                                            }, hour, minute, false);
+
+                                    timePickerDialog.show();
+                                }
+                            }, year, month, dayOfMonth);
+
+                    datePickerDialog.show();
+                }
+            });
+            descriptionEditText.setText(description);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+            Button addEventButton = findViewById(R.id.addEventButton);
         addEventButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -77,12 +202,12 @@ public class MainActivity extends AppCompatActivity {
         }
 
             try {
-                JSONObject eventJson = new JSONObject(jsonString);
-                String title = eventJson.getString("title");
-                String description = eventJson.getString("description");
-                String location = eventJson.getString("location");
-                String startDateTimeStr = eventJson.getString("startDateTime");
-                String endDateTimeStr = eventJson.getString("endDateTime");
+
+                String title = titleEditText.getText().toString();
+                String description = descriptionEditText.getText().toString();
+                String location = locationEditText.getText().toString();
+                String startDateTimeStr = startDatetimeEditText.getText().toString();
+                String endDateTimeStr =endDatetimeEditText.getText().toString();
 
                 SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss", Locale.US);
                 Date startDateTime = format.parse(startDateTimeStr);
@@ -108,9 +233,6 @@ public class MainActivity extends AppCompatActivity {
             } else {
                 Toast.makeText(this, "Calendar permission denied", Toast.LENGTH_SHORT).show();
             }
-        } catch (JSONException e) {
-            e.printStackTrace();
-            Toast.makeText(this, "Error parsing JSON", Toast.LENGTH_SHORT).show();
         } catch (ParseException e) {
             e.printStackTrace();
             Toast.makeText(this, "Error parsing date", Toast.LENGTH_SHORT).show();
